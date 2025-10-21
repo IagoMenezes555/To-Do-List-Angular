39a989ab6e1e517099ecf5f3256bf6f34e7abb03
Iago <iagomenezes555@gmail.com>
2025-10-21 16:16:01 -0300

    add: showing items in the todolist component on startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodolistComponent } from "./components/todolist/todolist.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodolistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Item } from '../../interfaces/item';
 
 @Component({
   selector: 'app-todolist',
   imports: [],
   templateUrl: './todolist.component.html',
-  styleUrl: './todolist.component.scss'
+  styleUrl: './todolist.component.scss',
 })
 export class TodolistComponent {
-
+  items: Item[] = [
+    { id: 1, title: 'Título 1', done: false },
+    { id: 2, title: 'Título 2', done: false }
+  ];
 }
