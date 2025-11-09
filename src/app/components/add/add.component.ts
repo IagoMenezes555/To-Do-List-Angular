5dad3181c83338ffa58c622f71a3314d6f0536fe
Iago <iagomenezes555@gmail.com>
2025-11-09 13:13:42 -0300

    add: saving tasks in localStorage

--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Item } from '../../interfaces/item';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add.component.html',
-  styleUrl: './add.component.scss'
+  styleUrl: './add.component.scss',
 })
 export class AddComponent {
+  @Output() itemEvent = new EventEmitter<Item>();
+  taskName: string = '';
 
+  newTask() {
+    const generatorId = Math.floor(Math.random() * 1000);
+    const task: Item = { id: generatorId, title: this.taskName, done: false };
+    this.itemEvent.emit(task);
+  }
 }
--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { Item } from '../../interfaces/item';
+import { AddComponent } from '../add/add.component';
 
 @Component({
   selector: 'app-todolist',
-  imports: [],
+  imports: [AddComponent],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.scss',
 })
 export class TodolistComponent {
-  items: Item[] = [
-    { id: 1, title: 'Título 1', done: false },
-    { id: 2, title: 'Título 2', done: false }
-  ];
+  items: Item[] = [];
+
+  constructor() {
+  }
+
+  received(task: Item) {
+    this.items.push(task);
+    const saveItems = JSON.stringify(this.items);
+    localStorage.setItem('tasks', saveItems);
+  }
 }
